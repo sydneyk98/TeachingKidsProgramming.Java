5e6dc7087eb823c820519545252ca9037c946fe4
Jacob Leiken <jacobleiken@gmail.com>
2013-06-18 15:23:00 -0700

    Another variation square -Jacob/Sydney

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
@@ -2,6 +2,7 @@ package org.teachingkidsprogramming.recipes;
 
 import org.teachingextensions.logo.Colors;
 import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.windows.MessageBox;
 
 public class SimpleSquare
 {
@@ -9,13 +10,13 @@ public class SimpleSquare
   {
     Tortoise.show();
     Tortoise.setSpeed(10);
-    int sides = 8;
+    int sides = MessageBox.askForNumericalInput("How many sides do you want?");
     for (int i = 1; i <= sides; i++)
     {
       Tortoise.setPenColor(Colors.getRandomColor());
-      Tortoise.setPenWidth(7);
-      Tortoise.move(75);
-      Tortoise.turn(360.0 / sides);
+      Tortoise.setPenWidth(i * 3.5);
+      Tortoise.move(i * 5);
+      Tortoise.turn(-360.0 * 3 / sides);
     }
   }
 }
