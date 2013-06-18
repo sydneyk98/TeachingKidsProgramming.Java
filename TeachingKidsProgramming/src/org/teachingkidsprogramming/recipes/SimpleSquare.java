89b4fe4c270d65d101ac5bda93ff80ca586243cb
Jacob Leiken <jacobleiken@gmail.com>
2013-06-18 14:07:28 -0700

    We Changed our Square! Jacob/Sydney is a loser

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
@@ -10,7 +10,18 @@ public class SimpleSquare
     Tortoise.show();
     //  Make the tortoise move as fast as possible --#6
     //  Do the following 4 times --#5.1
-    //      Change the color of the line the tortoise draws to "blue" --#4
+    Tortoise.setPenColor(Colors.Blues.Blue);
+    Tortoise.move(50);
+    Tortoise.turn(90);
+    Tortoise.setPenColor(Colors.Blues.Blue);
+    Tortoise.move(50);
+    Tortoise.turn(90);
+    Tortoise.setPenColor(Colors.Blues.Blue);
+    Tortoise.move(50);
+    Tortoise.turn(90);
+    Tortoise.setPenColor(Colors.Blues.Blue);
+    Tortoise.move(50);
+    Tortoise.turn(90);
     Tortoise.setPenColor(Colors.Blues.Blue);
     Tortoise.move(50);
     Tortoise.turn(90);
