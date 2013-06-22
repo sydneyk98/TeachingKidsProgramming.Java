5b061885b0592d8378a9be6cfa454c1ae7673f94
Sydney Kusumoputro <imnotsydney@gmail.com>
2013-06-22 13:40:16 -0700

    DigiFlower homework- Sydney Kusumoputro

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/DigiFlower.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/DigiFlower.java
@@ -1,36 +1,42 @@
 package org.teachingkidsprogramming.recipes;
 
+import org.teachingextensions.logo.ColorWheel;
+import org.teachingextensions.logo.Colors;
+import org.teachingextensions.logo.Tortoise;
+
 public class DigiFlower
 {
   public static void main(String[] args)
   {
-    //    Show the tortoise --#1
-    //    Make the tortoise move as fast as possible --#7
+    Tortoise.show();
+    Tortoise.setSpeed(10);
     //    Make the background silver --#8
-    //    Make the line the tortoise draws 3 pixels wide --#20
-    //    createColorPalette (recipe below) --#9
-    //    Do the following 15 times --#19
-    //      drawOctogon (recipe below) --#10
-    //      Turn the tortoise 1/15th of 360 degrees to the right --#18
-    //    Repeat
-    //    ------------- Recipe for createColorPalette --#9
-    //    Color 1 is red --#3
-    //    Color 2 is dark orange --#11
-    //    Color 3 is gold --#12
-    //    Color 4 is yellow --#13
-    //    Add color 1 to the color wheel --#3.1
-    //    Add color 2 to the color wheel --#11.1
-    //    Add color 3 to the color wheel --#12.1
-    //    Add color 4 to the color wheel --#13.1
-    //    Add color 4 to the color wheel --#14
-    //    Add color 3 to the color wheel --#15
-    //    Add color 2 to the color wheel --#16
-    //    Add color 1 to the color wheel --#17
-    //    ------------- Recipe for drawOctogon --#10
-    //    Do the following 8 times --#6
-    //      Change the color of the line the tortoise draws to the next color on the color wheel --#4
-    //      Move the tortoise 50 pixels --#2
-    //      Turn the tortoise 1/8th of 360 degrees to the right --#5
-    //    Repeat  
+    Tortoise.setPenWidth(3);
+    for (int i = 1; i <= 15; i++)
+    {
+      Tortoise.turn(360 / 15);
+      createColorPalette();
+      drawOctogan();
+    }
+  }
+  private static void drawOctogan()
+  {
+    for (int i = 1; i <= 8; i++)
+    {
+      Tortoise.setPenColor(ColorWheel.getNextColor());
+      Tortoise.move(50);
+      Tortoise.turn(360 / 8);
+    }
+  }
+  private static void createColorPalette()
+  {
+    ColorWheel.addColor(Colors.Reds.Red);
+    ColorWheel.addColor(Colors.Oranges.DarkOrange);
+    ColorWheel.addColor(Colors.Yellows.Gold);
+    ColorWheel.addColor(Colors.Yellows.Yellow);
+    ColorWheel.addColor(Colors.Yellows.Yellow);
+    ColorWheel.addColor(Colors.Yellows.Gold);
+    ColorWheel.addColor(Colors.Oranges.Orange);
+    ColorWheel.addColor(Colors.Reds.Red);
   }
 }
