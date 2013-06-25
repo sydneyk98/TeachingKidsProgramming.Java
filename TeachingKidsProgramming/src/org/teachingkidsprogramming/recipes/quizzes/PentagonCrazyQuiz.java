9b9c9c1a3392325e50f2d5eab7143f6b49ea822f
Sydney Kusumoputro <imnotsydney@gmail.com>
2013-06-25 16:00:34 -0700

    pentagon crazy quiz sydney and louis

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/quizzes/PentagonCrazyQuiz.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/quizzes/PentagonCrazyQuiz.java
@@ -1,29 +1,34 @@
 package org.teachingkidsprogramming.recipes.quizzes;
 
+import org.teachingextensions.logo.ColorWheel;
+import org.teachingextensions.logo.Colors;
+import org.teachingextensions.logo.Tortoise;
 import org.teachingkidsprogramming.recipes.quizzes.graders.PentagonCrazyQuizGrader;
 import org.teachingkidsprogramming.recipes.quizzes.graders.PentagonQuiz;
 
-public class PentagonCrazyQuiz extends PentagonCrazyQuizGrader
+public class PentagonCrazyQuiz extends PentagonQuiz
 {
-  //      Question1
-  //      Create a method called thread
-  //       that moves the tortoise 6 pixels
+  public void thread()
+  {
+    Tortoise.move(6);
+  }
   public void question2()
   {
-    //        Do the following 76 times
-    //        Quiz.Stitch()
-    //        Repeat
+    for (int i = 0; i < 76; i++)
+    {
+      stitch();
+    }
   }
   public void question3()
   {
-    //        Add lime to the color wheel
+    ColorWheel.addColor(Colors.Greens.Lime);
   }
   public void question4()
   {
-    //        Add red to the color wheel
+    ColorWheel.addColor(Colors.Reds.Red);
   }
   public static void main(String[] args)
   {
-    new PentagonCrazyQuizGrader().grade(new PentagonQuiz());
+    new PentagonCrazyQuizGrader().grade(new PentagonCrazyQuiz());
   }
 }
