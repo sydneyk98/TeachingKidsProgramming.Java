30bba2f73e50e8a1f6a737ab34b28bbe0fe31527
Sydney Kusumoputro <imnotsydney@gmail.com>
2013-06-25 18:17:01 -0700

    KnottedRing homework- Sydney

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/KnottedRing.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/KnottedRing.java
@@ -1,32 +1,40 @@
 package org.teachingkidsprogramming.recipes;
 
+import org.teachingextensions.logo.ColorWheel;
+import org.teachingextensions.logo.Colors;
+import org.teachingextensions.logo.Tortoise;
+
 public class KnottedRing
 {
   public static void main(String[] args)
   {
-    //      Make the tortoise move as fast as possible --#4
-    //      createColorPalette (recipe below) --#6
-    //      ------------- Recipe for createColorPalette --#6
-    //      Add hot pink to the color wheel --#6
-    //      Add red to the color wheel --#12
-    //      Add fuchsia to the color wheel --#13
-    //      Add orange red to the color wheel --#14
-    //      Add deep pink to the color wheel --#15
-    //      Add medium violet red to the color wheel --#16
-    //      Add crimson to the color wheel --#17
-    //      Add tomato to the color wheel --#18
-    //      ------------- End of createColorPalette recipe --#6
-    //      Do the following 30 times --#10
-    //              Change the color of the line the tortoise draws to a random color from the color wheel --#5
-    //              drawOctagonWithOverlap (recipe below) --#8
-    //              ------------- Recipe for drawOctagonWithOverlap --#7
-    //              Do the following 8 + 1 times --#3
-    //                    Move the tortoise 110 pixels --#1
-    //                    Turn the tortoise 1/8th of 360 degrees to the right --#2
-    //              Repeat 
-    //              ------------- End of drawOctagonWithOverlap recipe --#7
-    //       Turn the tortoise 1/30th of 360 degrees to the right --#9
-    //       Turn the tortoise 5 more degrees to the right --#11
-    //       Repeat 
+    createColorPalette();
+    Tortoise.setSpeed(10);
+    for (int i = 1; i <= 30; i++)
+    {
+      Tortoise.setPenColor(ColorWheel.getRandomColorFromWheel());
+      drawOctagonWithOverLap();
+      Tortoise.turn(360 / 30);
+    }
+    Tortoise.turn(5);
+  }
+  private static void createColorPalette()
+  {
+    ColorWheel.addColor(Colors.Pinks.HotPink);
+    ColorWheel.addColor(Colors.Reds.Red);
+    ColorWheel.addColor(Colors.Pinks.Fuchsia);
+    ColorWheel.addColor(Colors.Oranges.Orange);
+    ColorWheel.addColor(Colors.Pinks.DeepPink);
+    ColorWheel.addColor(Colors.Purples.MediumVioletRed);
+    ColorWheel.addColor(Colors.Reds.Crimson);
+    ColorWheel.addColor(Colors.Reds.Tomato);
+  }
+  private static void drawOctagonWithOverLap()
+  {
+    for (int i = 1; i <= 9; i++)
+    {
+      Tortoise.move(110);
+      Tortoise.turn(360 / 8);
+    }
   }
 }
